15aa94c1c0553a6c8a3373617fb7457ababbb6ac
gauravsfoundry <gauravsfoundry@gmail.com>
2022-12-19 21:38:43 +0530

    Changed the main class to instantiate BasketballCoach through the config file

--- src/main/java/com/personal/gaurav/springdemo/MyApp.java
+++ src/main/java/com/personal/gaurav/springdemo/MyApp.java
@@ -1,8 +1,29 @@
 package com.personal.gaurav.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class MyApp {
     public static void main(String[] args) {
-        Coach theCoach = new TrackCoach();
-        System.out.println(theCoach.getDailyWorkout());
+        //create application context which is an abstraction for container 
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        //retrieve a bean from the Spring container
+        /*second parameter (interface name) is optional. When we pass the interface to the method, behind the scenes Spring will cast the object.
+        However, there are some slight differences than normal casting. From the Spring docs: Behaves the same as getBean(String), but provides
+        a measure of type safety by throwing a BeanNotOfRequiredTypeException if the bean is not of the required type. This means that ClassCastException
+        can't be thrown on casting the result correctly, as can happen with getBean(String)*/
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+        
+        //call methods on the bean
+        System.out.println("theCoach.getDailyWorkout() = " + theCoach.getDailyWorkout());
+
+        //close the context
+        context.close();
     }
 }
+
+
+
+
+
