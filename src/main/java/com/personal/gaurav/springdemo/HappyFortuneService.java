4adc90371f618208cf512165ff8680f82ed6dc9b
gauravsfoundry <gauravsfoundry@gmail.com>
2023-01-06 15:15:15 +0530

    Randomizing the generation of predictions. Instead of returning only one hardcoded prediction, we are generating from an array randomly

--- src/main/java/com/personal/gaurav/springdemo/HappyFortuneService.java
+++ src/main/java/com/personal/gaurav/springdemo/HappyFortuneService.java
@@ -1,8 +1,16 @@
 package com.personal.gaurav.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
+
+    String[] fortuneList = {"Today is your lucky day!",
+                            "Today will be a challenging day",
+                            "Today will be a bad day, so be careful!"};
+    Random rand = new Random();
+
     @Override
     public String getFortune() {
-        return "Today is your lucky day!";
+        return fortuneList[rand.nextInt(3)];
     }
 }
