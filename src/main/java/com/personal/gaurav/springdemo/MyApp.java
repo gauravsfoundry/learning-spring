a4c69c5c68595756382973115d8059597587fb78
gauravsfoundry <gauravsfoundry@gmail.com>
2023-01-06 11:14:59 +0530

    Dependency Injection -> Setter Injection and injection of literal values with XML configuration

--- src/main/java/com/personal/gaurav/springdemo/MyApp.java
+++ src/main/java/com/personal/gaurav/springdemo/MyApp.java
@@ -2,6 +2,8 @@ package com.personal.gaurav.springdemo;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 
 public class MyApp {
     public static void main(String[] args) {
@@ -13,12 +15,14 @@ public class MyApp {
         However, there are some slight differences than normal casting. From the Spring docs: Behaves the same as getBean(String), but provides
         a measure of type safety by throwing a BeanNotOfRequiredTypeException if the bean is not of the required type. This means that ClassCastException
         can't be thrown on casting the result correctly, as can happen with getBean(String)*/
-        Coach theCoach = context.getBean("myCoach", Coach.class);
-        
+        TrackCoach theCoach = context.getBean("myCoach", TrackCoach.class); //Using direct class instead of interface here to cast. Class implements additional
+                                                                                  //methods which interface doesn't define so those methods won't be available to use
+                                                                                  //if we want to make use of them
         //call methods on the bean
         System.out.println("theCoach.getDailyWorkout() = " + theCoach.getDailyWorkout());
         System.out.println("theCoach.getDailyFortune() = " + theCoach.getDailyFortune());
-
+        System.out.println("theCoach.getEmailAddress = " + theCoach.getEmailAddress());
+        System.out.println("theCoach.getTeamName = " + theCoach.getTeamName());
         //close the context
         context.close();
     }
--- src/main/java/com/personal/gaurav/springdemo/TrackCoach.java
+++ src/main/java/com/personal/gaurav/springdemo/TrackCoach.java
@@ -2,6 +2,24 @@ package com.personal.gaurav.springdemo;
 
 public class TrackCoach implements Coach {
     private FortuneService fortuneService;
+    private String emailAddress;
+    private String teamName;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
 
     public void setFortuneService(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
