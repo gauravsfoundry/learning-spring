417c68af8711421ca4ad9868ede7fd6bf253c260
gauravsfoundry <gauravsfoundry@gmail.com>
2023-01-08 13:21:00 +0530

    Introducing Spring configuration with Java Annotations. Added a new class with annotations

--- src/main/java/com/personal/gaurav/springdemo/MyApp.java
+++ src/main/java/com/personal/gaurav/springdemo/MyApp.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 
 public class MyApp {
     public static void main(String[] args) {
+        
         //create application context which is an abstraction for container 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         
@@ -15,14 +16,11 @@ public class MyApp {
         However, there are some slight differences than normal casting. From the Spring docs: Behaves the same as getBean(String), but provides
         a measure of type safety by throwing a BeanNotOfRequiredTypeException if the bean is not of the required type. This means that ClassCastException
         can't be thrown on casting the result correctly, as can happen with getBean(String)*/
-        Coach firstCoach = context.getBean("myCoach", Coach.class);
-        Coach secondCoach = context.getBean("myCoach", Coach.class);
-        boolean sameBean = (firstCoach == secondCoach);
-
-        //No special scope defined in applicationContext.xml so default scope of singleton applies and only one instance of bean is created
-        System.out.println("Singleton Test: Is it same bean = " + sameBean);
-        System.out.println("firstCoach resides@ = " + firstCoach);
-        System.out.println("secondCoach resides@ = " + secondCoach);
+        Coach theCoach = context.getBean("myTennisCoach", Coach.class);
+        
+        //call methods on bean
+        System.out.println("theCoach.getDailyWorkout() = " + theCoach.getDailyWorkout());
+        System.out.println("theCoach.getDailyFortune() = " + theCoach.getDailyFortune());
 
         //close the context
         context.close();
---
+++ src/main/java/com/personal/gaurav/springdemo/TennisCoach.java
@@ -0,0 +1,18 @@
+package com.personal.gaurav.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("myTennisCoach") //Specifying bean id along with annotation. If omitted, bean id is class name with first letter converted to small case i.e. tennisCoach
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice forehand smash for 30 mins";
+    }
+
+    @Override
+    public String getDailyFortune() {
+
+        return "Toady is your lucky day";
+    }
+}
