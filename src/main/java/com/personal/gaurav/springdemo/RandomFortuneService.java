ee700fc4c8833cfc0c51386e8a02fb43e876afc0
gauravsfoundry <gauravsfoundry@gmail.com>
2023-01-10 17:05:56 +0530

    Spring configuration with Java Annotations. Converted RandomFortuneService to use lifecycle methods

--- src/main/java/com/personal/gaurav/springdemo/RandomFortuneService.java
+++ src/main/java/com/personal/gaurav/springdemo/RandomFortuneService.java
@@ -2,6 +2,8 @@ package com.personal.gaurav.springdemo;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,22 +12,30 @@ import java.util.Random;
 @Component
 public class RandomFortuneService implements FortuneService {
 
+    private String[] fortuneList;
     Random rand = new Random();
 
     //Read predictions from a file
-    public String[] readFortuneFromFile(){
+    @PostConstruct
+    public void readFortuneFromFile(){
         try {
+            System.out.println("Inside @PostConstruct method");
             //hard-coding the file path here for simplicity
             String dailyFortune = Files.readString(Path.of("/Users/23343/Gaurav/PersonalProjects/learning-spring/src/main/resources/Predictions.txt"));
-            return dailyFortune.split("\n");
+            fortuneList = dailyFortune.split("\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    @PreDestroy
+    public void closeFileAfterReading(){
+        System.out.println("Inside @PreDestroy method simulating file close");
+        System.out.println("File closed successfully!");
+    }
+
     @Override
     public String getFortune() {
-        String[] fortuneList = readFortuneFromFile();
         return fortuneList[rand.nextInt(fortuneList.length)];
     }
 }
